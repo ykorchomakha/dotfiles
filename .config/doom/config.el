377be2148595707fd6b4d3ffa373d08e75869316
Yurii Korchomakha <ykorchomakha@gmail.com>
2026-03-02 07:49:14 +0200

    update

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -21,8 +21,11 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 18)
-     doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 18))
+;; (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 18)
+;;      doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 18))
+
+(setq doom-font (font-spec :family "FantasqueSansM Nerd Font" :size 18)
+     doom-variable-pitch-font (font-spec :family "FantasqueSansM Nerd Font" :size 18))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
